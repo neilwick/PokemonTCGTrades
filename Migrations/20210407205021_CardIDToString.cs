6b09ca9107d8b5096ce0e70248c63f8be3e20d32
Neil Wick <neil@wick.net>
2021-04-07 16:52:16 -0400

    New migration

---
+++ Migrations/20210407205021_CardIDToString.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PokemonTCGTrades.Migrations
+{
+    public partial class CardIDToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardID",
+                table: "Listings",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<uint>(
+                name: "CardID",
+                table: "Listings",
+                type: "int unsigned",
+                nullable: false,
+                defaultValue: 0u,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+    }
+}
